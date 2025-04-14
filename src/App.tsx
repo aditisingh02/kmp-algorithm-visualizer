d98a4e08d905bf4eb1004a164a2988ec490766cb
Aditi Singh <aditi.singh0205@gmail.com>
2025-04-14 17:04:24 +0530

    add: kmp algorithm visualizer

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import KMPVisualizer from "./KMPVisualizer";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <KMPVisualizer />
+    </div>
+  );
+}
+
+export default App;
